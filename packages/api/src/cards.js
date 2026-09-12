0bfd74b49f39733dc3fd75d93734e8a91ed95a8d
Emad Albalas <emadalbalas@gmail.com>
2026-09-12 11:48:03 -0700

    no-mistakes(review): add OP16 and OP17 to FALLBACK_SET_CODES

--- packages/api/src/cards.js
+++ packages/api/src/cards.js
@@ -11,7 +11,7 @@ const SET_BASE_URL = 'https://raw.githubusercontent.com/hugoprudente/optcgjson/m
 
 const FALLBACK_SET_CODES = [
   'OP01', 'OP02', 'OP03', 'OP04', 'OP05', 'OP06', 'OP07', 'OP08',
-  'OP09', 'OP10', 'OP11', 'OP12', 'OP13', 'OP14', 'OP15',
+  'OP09', 'OP10', 'OP11', 'OP12', 'OP13', 'OP14', 'OP15', 'OP16', 'OP17',
   'ST01', 'ST02', 'ST03', 'ST04', 'ST05', 'ST06', 'ST07', 'ST08',
   'ST09', 'ST10', 'ST11', 'ST12', 'ST13', 'ST14', 'ST15', 'ST16',
   'ST17', 'ST18', 'ST19', 'ST20', 'ST21', 'ST22', 'ST23', 'ST24',
